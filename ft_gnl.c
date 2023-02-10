b1de071c7f1c819b6912e0da82e16198854c0840
Maya <mtrembla@student.42quebec.com>
2023-02-10 12:52:43 -0500

    add version of gnljoin without strlen

--- ft_gnl.c
+++ ft_gnl.c
@@ -6,7 +6,7 @@
 /*   By: mtrembla <mtrembla@student.42quebec>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/15 12:49:04 by mtrembla          #+#    #+#             */
-/*   Updated: 2023/01/30 11:29:40 by mtrembla         ###   ########.fr       */
+/*   Updated: 2023/02/10 12:50:12 by mtrembla         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,16 @@
 char	*ft_gnljoin(char *s, char c)
 {
 	char	*out;
-	int		n;
+	int		len;
 
-	n = ft_strlen(s) + 2;
-	out = malloc(sizeof(char) * n);
-	out[--n] = '\0';
-	out[--n] = c;
+	len = ft_strlen(s) + 2;
+	out = malloc(sizeof(char) * len);
+	out[--len] = '\0';
+	out[--len] = c;
 	if (s)
 	{
-		while (n--)
-			out[n] = s[n];
+		while (len--)
+			out[len] = s[len];
 		free(s);
 	}
 	return (out);
@@ -47,3 +47,28 @@ char	*ft_gnl(int fd)
 	}
 	return (line);
 }
+
+/*
+Alternative version of ft_gnljoin,
+use if strlen doesn't return 0 on NULL string.
+char	*ft_gnljoin(char *s, char c)
+{
+    char    *out;
+    int        n;
+
+    n = 2;
+    if (s)
+        while (*s && s++)
+            n++;
+    out = malloc(n * sizeof(char));
+    out[--n] = '\0';
+    out[--n] = c;
+    if (s)
+    {
+        while (n--)
+            out[n] = *--s;
+        free(s);
+    }
+    return (out);
+}
+*/
